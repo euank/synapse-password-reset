21fae052d52624ee0e7532c009c4320e9a5ab30d
Euan Kemp <euank@euank.com>
2016-11-29 23:55:29 -0800

    integ tests: gate behind a feature flag

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,47 +1,50 @@
-extern crate hyper;
-extern crate url;
+#[cfg(feature = "integ-tests")]
+mod integ_tests {
+    extern crate hyper;
+    extern crate url;
 
-use url::form_urlencoded;
-use hyper::Client;
+    use self::url::form_urlencoded;
+    use self::hyper::Client;
 
-fn register_user(c: &hyper::Client, uname: String, password: String) {
-    let encoded: String = form_urlencoded::Serializer::new(String::new())
-        .append_pair("Username", uname.as_ref())
-        .append_pair("Password", password.as_ref())
-        .finish();
-    let res = c.post("http://localhost:8082").body(encoded.as_bytes()).send();
+    fn register_user(c: &hyper::Client, uname: String, password: String) {
+        let encoded: String = form_urlencoded::Serializer::new(String::new())
+            .append_pair("Username", uname.as_ref())
+            .append_pair("Password", password.as_ref())
+            .finish();
+        let res = c.post("http://localhost:8082").body(encoded.as_bytes()).send();
 
-    res.unwrap();
-}
+        res.unwrap();
+    }
 
-fn reset_password(c: &hyper::Client, uname: String, token: String, password: String) -> hyper::status::StatusCode {
-    let encoded = form_urlencoded::Serializer::new(String::new())
-        .append_pair("username", uname.as_ref())
-        .append_pair("token", token.as_ref())
-        .append_pair("password", password.as_ref())
-        .finish();
-    let res = c.post("http://localhost:6767").body(encoded.as_bytes()).send();
+    fn reset_password(c: &hyper::Client, uname: String, token: String, password: String) -> hyper::status::StatusCode {
+        let encoded = form_urlencoded::Serializer::new(String::new())
+            .append_pair("username", uname.as_ref())
+            .append_pair("token", token.as_ref())
+            .append_pair("password", password.as_ref())
+            .finish();
+        let res = c.post("http://localhost:6767").body(encoded.as_bytes()).send();
 
-    res.unwrap().status
-}
+        res.unwrap().status
+    }
 
 #[test]
-fn it_works() {
-    let c = Client::new();
-    let orig_pass = "password1234".to_string();
+    fn it_works() {
+        let c = Client::new();
+        let orig_pass = "password1234".to_string();
 
-    register_user(&c, "bob".to_string(), orig_pass.clone());
+        register_user(&c, "bob".to_string(), orig_pass.clone());
 
-    // TODO: login with the above username and password
+        // TODO: login with the above username and password
 
-    let token = "abcd".to_string();
-    let full_uname = "bob:synapse_password_reset.local".to_string();
-    let new_pass = "newpassword123".to_string();
-    // Fail to reset password due to this token not being in the token-db yet
-    let sc = reset_password(&c, full_uname, token, new_pass);
-    assert!(sc.is_client_error());
-    
-    // TODO login with new password
+        let token = "abcd".to_string();
+        let full_uname = "bob:synapse_password_reset.local".to_string();
+        let new_pass = "newpassword123".to_string();
+        // Fail to reset password due to this token not being in the token-db yet
+        let sc = reset_password(&c, full_uname, token, new_pass);
+        assert!(sc.is_client_error());
 
-    // TODO verify login with old password fails
+        // TODO login with new password
+
+        // TODO verify login with old password fails
+    }
 }
